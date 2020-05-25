f7a5fefddf018aa70f0c5d066e716d891c0f2cf1
felixhaeberle <kontakt@felixhaeberle.de>
2020-05-25 22:41:22 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+@include_once __DIR__ . '/vendor/autoload.php';
+@include_once __DIR__ . '/src/webp.php';
+
+Kirby::plugin('felixhaeberle/kirby3-webp', [
+    'hooks' => [
+        'file.create:after' => function ($file) {
+            if ($this->option('kirby3-webp', false)) {
+                (new WebP\Convert)->generateWebP($file);
+            }
+        },
+    ],
+]);
---
+++ lib/webp.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace WebP;
+
+use Kirby\Cms\App as Kirby;
+use WebPConvert\WebPConvert;
+
+class Convert
+{
+    protected $quality;
+    protected $strip;
+    protected $serve;
+    protected $log;
+    protected $converters;
+
+    public function __construct()
+    {
+        $this->quality = kirby()->option('kirby3-webp.quality', 90);
+        $this->maxQuality = kirby()->option('kirby3-webp.maxQuality', 85);
+        $this->defaultQuality = kirby()->option('kirby3-webp.defaultQuality', 85);
+        $this->metadata = kirby()->option('kirby3-webp.metadata', "none");
+        $this->encoding = kirby()->option('kirby3-webp.encoding', "auto");
+        $this->skip = kirby()->option('kirby3-webp.skip', false);
+    }
+
+    public function generateWebP($file)
+    {
+        try {
+            // Checking file type since only images are processed
+            if ($file->type() == 'image') {
+                // WebPConvert options
+                $path = kirby()->root('content') . '/' . dirname($file->id()) . '/';
+                $input = $path . $file->filename();
+                $output = $path . $file->name() . '.webp';
+
+                // Generating WebP image & placing it alongside the original version
+                WebPConvert::convert($input, $output, $option = [
+                    'quality' => $this->quality,
+                    'max-quality' => $this->maxQuality,
+                    'default-quality' => $this->defaultQuality,
+                    'metadata' => $this->metadata,
+                    'encoding' => $this->encoding,
+                    'skip' => $this->skip,
+                ]);
+            }
+        } catch (Exception $e) {
+            return response::error($e->getMessage());
+        }
+    }
+}
