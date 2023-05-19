c4c36ea30259986a3c9ab6f012bf6cb77aa9553f
Michael van Laar <michael@van-laar.de>
2023-05-19 22:29:33 +0200

    🔨 refactor(index.php): extract functions to improve code readability and maintainability The code has been refactored to extract the functionality of generating and deleting webp files into separate functions. This improves the readability and maintainability of the code by making it easier to understand and modify. The extracted functions are `shouldGenerateWebP`, `generateWebP`, `deleteWebPFiles`, and `deleteIfExist`.

--- index.php
+++ index.php
@@ -3,27 +3,41 @@
 @include_once __DIR__ . '/vendor/autoload.php';
 @include_once __DIR__ . '/src/webp.php';
 
+function shouldGenerateWebP($file) {
+    return $file->kirby()->option('kirby3-webp', false) && $file->extension() !== 'svg';
+}
+
+function generateWebP($file) {
+    (new WebP\Convert)->generateWebP($file);
+}
+
+function deleteWebPFiles($file) {
+    $webpFile = dirname($file->root()) . '/' . $file->name() . '.webp';
+    $webpTxtFile = dirname($file->root()) . '/' . $file->name() . '.webp.txt';
+    deleteIfExist($webpFile);
+    deleteIfExist($webpTxtFile);
+}
+
+function deleteIfExist($file) {
+    if (F::exists($file)) {
+        F::remove($file);
+    }
+}
+
 Kirby::plugin('felixhaeberle/kirby3-webp', [
     'hooks' => [
         'file.create:after' => function ($file) {
-            if ($this->option('kirby3-webp', false) && $file->extension() !== 'svg') {
-                (new WebP\Convert)->generateWebP($file);
+            if (shouldGenerateWebP($file)) {
+                generateWebP($file);
             }
         },
         'file.replace:after' => function ($newFile, $oldFile) {
-            if ($this->option('kirby3-webp', false) && $newFile->extension() !== 'svg') {
-                (new WebP\Convert)->generateWebP($newFile);
+            if (shouldGenerateWebP($newFile)) {
+                generateWebP($newFile);
             }
         },
         'file.delete:after' => function ($file) {
-            $webpFile = dirname($file->root()) . '/' . $file->name() . '.webp';
-            $webpTxtFile = dirname($file->root()) . '/' . $file->name() . '.webp.txt';
-            if (F::exists($webpFile)) {
-                F::remove($webpFile);
-            }
-            if (F::exists($webpTxtFile)) {
-                F::remove($webpTxtFile);
-            }
+            deleteWebPFiles($file);
         },
     ],
 ]);
