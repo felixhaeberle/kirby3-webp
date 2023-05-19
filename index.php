883c99df9c4d053fbb381fcc339bd7f6b4788e39
Michael van Laar <michael@van-laar.de>
2023-05-19 19:52:17 +0200

    🐛 fix(index.php): add check for svg files to avoid generating webp files for them The plugin was generating webp files for all file types, including svg files. This commit adds a check to avoid generating webp files for svg files.

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 Kirby::plugin('felixhaeberle/kirby3-webp', [
     'hooks' => [
         'file.create:after' => function ($file) {
-            if ($this->option('kirby3-webp', false)) {
+            if ($this->option('kirby3-webp', false) && $file->extension() !== 'svg') {
                 (new WebP\Convert)->generateWebP($file);
             }
         },
         'file.replace:after' => function ($newFile, $oldFile) {
-            if ($this->option('kirby3-webp', false)) {
+            if ($this->option('kirby3-webp', false) && $newFile->extension() !== 'svg') {
                 (new WebP\Convert)->generateWebP($newFile);
             }
         },
